b44b9274b6e8b884376abc906cd0e4bf1c3c2352
Мирлан Садамбеков <sadambekovmirlan9@gmail.com>
2023-03-22 21:46:51 +0600

    ghhgh

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # This is a sample Python script.
 import decouple
 
-TOKEN = ''
+TOKEN = 'jfgnibhr'
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
